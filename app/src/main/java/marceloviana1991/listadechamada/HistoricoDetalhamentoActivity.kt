a810b0c2cff19fb45ee529cadeb9bc92d4d02289
marcelo <marceloviana1991@gmail.com>
2025-02-20 12:32:43 -0300

    historico

--- app/src/main/java/marceloviana1991/listadechamada/HistoricoDetalhamentoActivity.kt
+++ app/src/main/java/marceloviana1991/listadechamada/HistoricoDetalhamentoActivity.kt
@@ -1,24 +1,20 @@
 package marceloviana1991.listadechamada
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import marceloviana1991.listadechamada.databinding.ActivityHistoricoBinding
-import java.time.format.DateTimeFormatter
+import marceloviana1991.listadechamada.databinding.ActivityHistoricoDetalhamentoBinding
 
-class HistoricoActivity : AppCompatActivity() {
+class HistoricoDetalhamentoActivity : AppCompatActivity() {
 
     private val biding by lazy {
-        ActivityHistoricoBinding.inflate(layoutInflater)
+        ActivityHistoricoDetalhamentoBinding.inflate(layoutInflater)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,21 +25,16 @@ class HistoricoActivity : AppCompatActivity() {
             insets
         }
 
-        val historico = mutableListOf<String>()
-        val formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+        val position = intent.extras?.getInt("HISTORICO")
+        val chamada = ChamadaDao.buscaTodas()
+        val historicoDetalhamento = position?.let { chamada.get(it) }?.ausentes
 
-        ChamadaDao.buscaTodas().forEach {
-            historico.add(it.dateTime.format(formato) + " - " + it.codigoDoResponsavel)
+        val adapter = historicoDetalhamento?.let {
+            ArrayAdapter(
+                this, android.R.layout.simple_list_item_1, it.toArray()
+            )
         }
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, historico)
 
         biding.listView.adapter = adapter
-
-        biding.listView.setOnItemClickListener { adapterView, view, position, id ->
-            val intent = Intent(this, HistoricoDetalhamentoActivity::class.java)
-            intent.putExtra("HISTORICO", position)
-            startActivity(intent)
-        }
     }
 }
